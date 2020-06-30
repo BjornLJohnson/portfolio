81b72fec32bedcad1c9b70830896cca6cf1462aa
Bjorn <bjorn@lundjohnson.com>
2020-06-30 14:45:40 -0700

    Changed api

--- simpleScript.js
+++ simpleScript.js
@@ -4,31 +4,32 @@ theContent = {
     "key1": "hello",
     "key2": "world",
     "key3": "amazon"
-  }
+}
 
 
 $(document).ready(function () {
     $("#theButton").click(sendRequest);
+
+    function sendRequest(){
+        $.ajax({
+            method: 'POST',
+            url: 'https://8x6sxe620f.execute-api.us-west-2.amazonaws.com',
+            headers: {
+                Authorization: authToken
+            },
+            data: JSON.stringify(theContent),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error executing lambda function: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when processing your request :\n' + jqXHR.responseText);
+            }
+        });
+    };
+    
+    function completeRequest(result) {
+        console.log('Response received from API: ', result);
+    }
 }); 
 
-function sendRequest(){
-    $.ajax({
-        method: 'POST',
-        url: 'https://nj02nnic77.execute-api.us-west-1.amazonaws.com/prod',
-        headers: {
-            Authorization: authToken
-        },
-        data: JSON.stringify(theContent),
-        contentType: 'application/json',
-        success: completeRequest,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
-        }
-    });
-};
-
-function completeRequest(result) {
-    console.log('Response received from API: ', result);
-}
