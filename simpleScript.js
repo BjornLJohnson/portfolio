4231abc704cbf91dc472da63d37d84f308ed70e1
Bjorn <bjorn@lundjohnson.com>
2020-07-06 17:50:04 -0700

    Added dynamodb integration

--- simpleScript.js
+++ simpleScript.js
@@ -1,20 +1,42 @@
-var value = 2000;
-authToken = "";
-theContent = {
-    "key1": "hello",
-    "key2": "world",
-    "key3": "amazon"
+apiURL = "https://2vw37146i3.execute-api.us-west-2.amazonaws.com/test"
+
+dummyPostContent = {
+    "name": "test",
+    "type": "supplier",
+    "key2": "more info",
+    "key3": "even more information"
 }
 
 
 $(document).ready(function () {
-    $("#theButton").click(sendRequest);
+    $("#getButton").click(sendGetRequest);
 
-    function sendRequest(){
+    function sendGetRequest(){
         $.ajax({
             type: 'GET',
-            url: 'https://7silg83b94.execute-api.us-west-2.amazonaws.com/prod/',
-            success: completeRequest
+            url: apiURL,
+            success: completeGetRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error executing lambda function: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when processing your request :\n' + jqXHR.responseText);
+            }
+        });
+    };
+    
+    function completeGetRequest(result) {
+        console.log('Response received from API: ', result);
+        $(getResponse).text(result);
+    }
+
+    $("#postButton").click(sendPostRequest);
+
+    function sendPostRequest(){
+        $.ajax({
+            type: 'POST',
+            url: apiURL,
+            data: JSON.stringify(dummyPostContent),
+            success: completePostRequest
 //             error: function ajaxError(jqXHR, textStatus, errorThrown) {
 //                 console.error('Error executing lambda function: ', textStatus, ', Details: ', errorThrown);
 //                 console.error('Response: ', jqXHR.responseText);
@@ -23,9 +45,9 @@ $(document).ready(function () {
         });
     };
     
-    function completeRequest(result) {
+    function completePostRequest(result) {
         console.log('Response received from API: ', result);
-        $(serverResponse).text("I heard: " + result);
+        $(postResponse).text(result);
     }
 }); 
 
